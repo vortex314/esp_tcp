6bf18a0aa0604b1c000a3350e6c6556fdc21a845
vortex314 <lieven.merckx@gmail.com>
2016-04-24 20:09:02 +0200

    tag

---
+++ cpp/DWM1000_Tag.h
@@ -0,0 +1,25 @@
+/*
+ * DWM1000_Tag.h
+ *
+ *  Created on: Feb 12, 2016
+ *      Author: lieven
+ */
+
+#ifndef DWM1000_Tag_H_
+#define DWM1000_Tag_H_
+
+#include <Handler.h>
+
+class DWM1000_Tag: public Handler {
+	uint32_t _count;
+public:
+	DWM1000_Tag();
+	virtual ~DWM1000_Tag();
+	void mode(uint32_t m);
+	void init();
+	void resetChip();
+	void initSpi();
+	bool dispatch(Msg& msg);
+};
+
+#endif /* DWM1000_Tag_H_ */
